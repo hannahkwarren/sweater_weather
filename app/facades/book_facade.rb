5828e7bb0c2faacd41aa14635e272f4edb66c3d0
Hannah Warren <hannahkwarren@gmail.com>
2022-03-07 12:38:31 -0500

    Add Facade tests with fixtures

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,32 +1,23 @@
 # app/facades/book_facade.rb
 class BookFacade
   def self.location_books(location, quantity)
-    attributes = books_data_parse(location, quantity)
-    # Book.new(attributes)
+    parsed_json = get_books_data(location, quantity)
+
+   x = {  destination: location,
+          forecast: current_forecast_data(location),
+          total_books_found: parsed_json[:numFound],
+          books:
+            parsed_json[:docs].each do |doc|
+              { isbn: doc[:isbn],
+                title: doc[:title],
+                publisher: doc[:publisher] }
+            end }
   end
 
   def self.get_books_data(location, quantity)
     # if quantity.class != Integer
     # end
     BookService.get_books(location, quantity)
-  end
-
-  def self.books_data_parse(location, quantity)
-    parsed_json = get_books_data(location, quantity)
-
-    { 
-      destination: location,
-      forecast: current_forecast_data(location),
-      total_books_found: parsed_json[:numFound],
-      books: [
-        parsed_json[:docs].each do |doc|
-          { isbn: doc[:isbn],
-            title: doc[:title],
-            publisher: doc[:publisher]
-          }
-        end
-        ]
-    }
   end
 
   def self.current_forecast_data(location)
--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -1,17 +1,20 @@
 # spec/facades/book_facade_spec.rb
 require 'rails_helper'
 RSpec.describe BookFacade do
-
-  it '::get_books_data' do
-    books_api_data = BookFacade.get_books_data('Trenton')
+  it '::get_books_data', :vcr do
+    books_api_data = BookFacade.get_books_data('Trenton', 5)
     expect(books_api_data).to be_a Hash
   end
 
-  it '::books_data_parse' do
-    final_data = BookFacade.location_books('Philadelphia', 8)
-    parsed = BookFacade.books_data_parse('Philadelphia', 8)
-    expect(parsed).to be_a Hash
+  it '::current_forecast_data', :vcr do
+    forecast_summary_info = BookFacade.current_forecast_data('Poughkeepsie,NY')
+    expect(forecast_summary_info).to be_a Hash
   end
 
-  
+  it '::location_books', :vcr do
+    books_response = BookFacade.location_books('Pittsburgh', 7)
+    expect(books_response).to be_a Hash
+    expect(books_response[:books]).to be_an Array
+    expect(books_response[:books].count).to eq(7)
+  end
 end
