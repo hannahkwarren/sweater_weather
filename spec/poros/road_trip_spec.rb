fd9ea584090e66e26b27362ae0ee40769ab53be2
Hannah Warren <hannahkwarren@gmail.com>
2022-03-09 12:32:09 -0500

    Shore up testing after late-night project submission

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe RoadTrip do
     attributes = {
       start_city: 'Denver, CO',
       end_city: 'Boulder, CO',
-      travel_time: 2240,
+      travel_time: [0, 2240],
       weather_at_eta: 'snow and sleet'
     }
 
@@ -14,7 +14,7 @@ RSpec.describe RoadTrip do
     expect(trip).to be_a RoadTrip
     expect(trip.start_city).to eq('Denver, CO')
     expect(trip.end_city).to eq('Boulder, CO')
-    expect(trip.travel_time).to eq(0.62)
+    expect(trip.travel_time).to eq("0 hours, 37 minutes")
     expect(trip.weather_at_eta).to eq('snow and sleet')
   end
 end
